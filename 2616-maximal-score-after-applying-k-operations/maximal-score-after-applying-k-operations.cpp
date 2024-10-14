2b38d272c79b049adf6192aa363fab64cf07f039
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-10-14 18:44:32 +0530

    Time: 214 ms (12.47%) | Memory: 79.6 MB (71.57%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    long long maxKelements(vector<int>& nums, int k) {
-        priority_queue<int> pqmax;
-        for(int i = 0; i < nums.size(); i++) {
-            pqmax.push(nums[i]);
-        }        
-        long long ans = 0; 
-        while(k) {
-            long long x = pqmax.top();
-            pqmax.pop();
-            ans = ans + x;
+    long long maxKelements(std::vector<int>& nums, int k) {
+        // Create a max heap using priority_queue
+        std::priority_queue<int> pq;
+        
+        // Add elements to the priority queue
+        for (int x : nums) {
+            pq.push(x);
+        }
+        
+        long long score = 0;
+        
+        // Process the queue while there are elements and k is greater than 0
+        while (!pq.empty() && k > 0) {
+            int x = pq.top();
+            pq.pop();
+            score += x;
             
-
-            x = ceil(static_cast<double>(x) / 3);
-            cout << x << " ";
-            pqmax.push(x);
+            // Add the next element as the ceiling of x/3
+            int newElement = static_cast<int>(std::ceil(x / 3.0));
+            pq.push(newElement);
+            
             k--;
         }
-        return ans;
+        
+        return score;
     }
 };
