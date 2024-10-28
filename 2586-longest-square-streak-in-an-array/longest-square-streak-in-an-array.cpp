a74b9db36e4599feff74b742bc7304c28638c8fd
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-10-28 20:12:34 +0530

    Time: 471 ms (6.67%) | Memory: 255 MB (6.43%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_set<long long> h(nums.begin(), nums.end());
-        sort(nums.begin(), nums.end());
-        int max_count = -1;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int count = 1;
-            long long current = nums[i];
-            
-            while (h.find(current * current) != h.end()) {
-                current *= current;
-                count++;
-            }
-            
-            max_count = max(max_count, count);
+        unordered_set<long long> h;
+        for(int i=0;i<nums.size();i++){
+            h.insert(nums[i]);
         }
-        
-        return max_count > 1 ? max_count : -1;
+        vector<vector<long long>>ans;
+      //  sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++){
+            vector<long long> v;
+            v.push_back(nums[i]);
+            while(1){
+                long long squared = static_cast<long long>(nums[i]) * nums[i];
+                if(h.find(squared)!=h.end()){
+                    v.push_back(nums[i]*nums[i]);
+                    nums[i]=nums[i]*nums[i];
+                }
+                else break;
+            }
+            ans.push_back(v);
+        }
+        long long ok=INT_MIN;
+        for(auto i: ans){
+            ok=max(ok,static_cast<long long>(i.size()));
+            
+        }
+        if(ok==1) return -1;
+        return ok;
     }
 };
