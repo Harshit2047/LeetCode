62f01636e025697908f5447fb218aae55855fe20
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-05 19:27:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (27.82%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int healper(vector<int> &cost,int idx,int size,vector<int> &dp){
-        if(idx>=size) return 0;
-        if(idx==size-1) return cost[idx];
-        if(dp[idx]!=-1) return dp[idx];
-        return dp[idx] = (cost[idx]+min(healper(cost,idx+1,size,dp),healper(cost,idx+2,size,dp)));
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int> dp(n+1,-1);
-        return min(healper(cost,0,n,dp),healper(cost,1,n,dp)) ;
+        int n = cost.size();
+        vector<int> v(cost.size());
+        v[0]=cost[0];
+        if(n>1) v[1]=cost[1];
+        for(int i=2;i<=cost.size()-1;i++){
+            v[i]=cost[i]+min(v[i-1],v[i-2]);
+        }
+    return min(v[n - 1], v[n - 2]);
+        
     }
 };
