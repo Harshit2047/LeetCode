dacf2fba4ef80e6ca8caef80f48e58b5271d8b8f
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-05 23:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (45.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int healper(vector<int> & nums,int idx,vector<int> &dp){
-        if(idx>nums.size()-1) return 0;
-        if(idx==nums.size()-1) return nums[idx];
-        if(dp[idx]!=-1) return dp[idx];
-        return  dp[idx] = max(nums[idx]+healper(nums,idx+2,dp),healper(nums,idx+1,dp));
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-       return  healper(nums,0,dp);
+        vector<int> v(n);
+       v[0]=nums[0];
+     if(n>1)  v[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            v[i]=max(nums[i]+v[i-2],v[i-1]);
+        }
+        return v[n-1];
     }
 };
