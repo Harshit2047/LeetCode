301536629368852d199504585ab7406b4fe8441d
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-05 20:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (14.18%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int healper(int n,vector<int> &dp){
-        if(n<0) return 0;
-        if(n==0 || n==1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = healper(n-1,dp)+healper(n-2,dp)+healper(n-3,dp);
-    }
     int tribonacci(int n) {
-        vector<int> dp(n+1,-1);
-        return healper(n,dp);
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 1;
+        if(n==3) return 2;
+       vector<int> v(n+1);
+       v[0]=0;
+       v[1]=1;
+       v[2]=1;
+       for(int i=3;i<=n;i++){
+        v[i]=v[i-1]+v[i-2]+v[i-3];
+       }
+       return v[n];
+
     }
 };
