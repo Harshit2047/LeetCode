d5e29b309118ffdc4a9571d3f917b2441550f014
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-06 01:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (54.99%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int healper(vector<int> &nums,int idx,int size,vector<int> &dp){
-        if(idx>=size) return 0;
-        if(idx==size-1) return nums[idx];
-        if(dp[idx]!=-1) return dp[idx];
-        int take=nums[idx]+healper(nums,idx+2,size,dp);
-        int leave=healper(nums,idx+1,size,dp);
-        return dp[idx] = max(leave,take);
-    }
     int rob(vector<int>& nums) {
         
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp(n,-1);
-        int includingFirst=healper(nums,0,n-1,dp);
-        fill(dp.begin(), dp.end(), -1);
-        int excludingFirst=healper(nums,1,n,dp);
-        return max(includingFirst,excludingFirst);
+        vector<int> including(n);
+        including[0]=nums[0];
+     if(n>1) including[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n-1;i++){
+            including[i]=max(nums[i]+including[i-2],including[i-1]);
+        }
+        vector<int> excluding(n);
+       
+     if(n>1)    excluding[1]=nums[1];
+     if(n>2)   excluding[2]=max(nums[1],nums[2]);
+        for(int i=3;i<n;i++){
+            excluding[i]=max(nums[i]+excluding[i-2],excluding[i-1]);
+        }
+
+        return max(excluding[n-1],including[n-2]);
     }
 };
