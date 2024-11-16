6ccfa3e523f562ad044d9d6dbb0fcdd10112169e
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-17 00:40:06 +0530

    Time: 365 ms (27.72%) | Memory: 155.9 MB (35.43%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -4,8 +4,10 @@ public:
         if (idx == nums.size()) {
             return target == 0 ? 0 : INT_MIN;
         }
+        if(target==0) return 0;
         if(dp[idx][target]!=-1) return dp[idx][target];
         int skip = helper(nums, target, idx + 1,dp);
+
         if(target<nums[idx]) return dp[idx][target]= skip;
         int take = 1+helper(nums, target - nums[idx], idx + 1,dp);
         
