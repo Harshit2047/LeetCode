8877c31d9d46b163ad3cdea799b6aa06febdd245
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-18 20:40:32 +0530

    Time: 315 ms (5.02%) | Memory: 10.4 MB (60.07%) - LeetSync

---
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        vector<int> v;
+        int n=code.size();
+        for(int i=0;i<code.size();i++){
+            int j;
+            if(k>=0) j=(i+1)%code.size();
+            if(k<0)  j=(i-1+n)%code.size();
+            int times=abs(k);
+            int sum=0;
+            while(times>0){
+                int idx=(j+n)%code.size();
+                cout<<idx<<" ";
+                sum=sum+code[idx];
+                if(k>=0)j++;
+                if(k<0) j--;
+                times--;
+            }
+            v.push_back(sum);    
+        }
+        return v;
+    }
+};
