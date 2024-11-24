cfd2d3884bd0cdc50624be3924e0fe8999331639
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-11-25 02:06:36 +0530

    Time: 43 ms (23.75%) | Memory: 26.1 MB (30.28%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string &text1, string &text2,int m,int n,int memo[][1001]) {
-        if(m==0 || n==0){
-            return 0;
-        }
-        if(memo[m][n]!=-1){
-            return memo[m][n];
-        }
-        if(text1[m-1]==text2[n-1]){
-            return memo[m][n]= 1+longestCommonSubsequence(text1,text2,m-1,n-1,memo);
-        }else{
-            return memo[m][n]= max(longestCommonSubsequence(text1,text2,m-1,n,memo),longestCommonSubsequence(text1,text2,m,n-1,memo));
-        }
+    vector<vector<int>> dp;
+    int lcs(int i,int j,string &a, string &b)
+    {
+        if(i==-1 || j==-1)    return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+        
+        if(a[i] == b[j])    return dp[i][j] = 1+lcs(i-1,j-1,a,b);
+        return dp[i][j] = max(lcs(i-1,j,a,b), lcs(i,j-1,a,b));
     }
-    
     int longestCommonSubsequence(string text1, string text2) {
-        int m=text1.size(),n=text2.size();
-        int memo[1001][1001];
-        for(int i=0;i<1001;i++){
-            for(int j=0;j<1001;j++){
-                memo[i][j]=-1;
-            }
-        }
-        return longestCommonSubsequence(text1,text2,m,n,memo);
+        int n1 = text1.size();
+        int n2 = text2.size();
+        dp.resize(n1,vector<int>(n2,-1));
+        
+        return lcs(n1-1,n2-1,text1,text2);
+      
     }
 };
