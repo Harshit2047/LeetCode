e058c267e2cae87e0f757be082e2046094ce3265
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-12-25 22:59:37 +0530

    Time: 280 ms (12.08%) | Memory: 14 MB (22.12%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    bool isPer(int n){
-        int ok=sqrt(n);
-        return ok*ok==n;
+    bool isPerfect(int n){
+        int x=sqrt(n);
+        return x*x==n;
     }
-    int helper(int n ,vector<int> &dp){
-        if(isPer(n)==true) return 1;
-        int ok=INT_MAX;
+    int helper(int n,vector<int> &dp){
+        if(isPerfect(n)) return 1;
         if(dp[n]!=-1) return dp[n];
+        int ans=INT_MAX;
         for(int i=1;i*i<=n;i++){
-            int count=helper(i*i,dp)+helper(n-i*i,dp);
-             ok=min(ok,count);
+            int ways=helper(i*i,dp)+helper(n-i*i,dp);
+            ans=min(ways,ans);
         }
-
-        return dp[n]=ok;
+        return dp[n]=ans;
     }
     int numSquares(int n) {
         vector<int> dp(n+1,-1);
-        return helper(n,dp);
+       return helper(n,dp);
     }
 };
