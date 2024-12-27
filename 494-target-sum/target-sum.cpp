784316a9cedb039953785cd804a29b3793e301d8
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-12-27 19:56:27 +0530

    Time: 14 ms (61.28%) | Memory: 26.3 MB (25.12%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    long long healper(vector<int> &nums,int target,int idx,int val,vector<vector<int>> &dp,int sum){
-        if(idx==nums.size()){
-            if(target==val) return 1;
-            else return 0; 
+    int helper(vector<int> &nums,int target,int idx,int size,int sum,int total,vector<vector<int>> &dp){
+        if(idx==size){
+            if(target==sum) return 1;
+            else return 0;
         }
-        if(dp[idx][val+sum]!=-1) return dp[idx][val+sum];
-        return dp[idx][val+sum]=healper(nums,target,idx+1,val+nums[idx],dp,sum)+healper(nums,target,idx+1,val-nums[idx],dp,sum);
-        
-        
+        if(dp[idx][sum+total]!=-1) return dp[idx][sum+total];
+        int postive=helper(nums,target,idx+1,size,sum+nums[idx],total,dp);
+        int negative=helper(nums,target,idx+1,size,sum-nums[idx],total,dp);
+        return dp[idx][sum+total]=(negative+postive);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        for(auto i:nums) sum=sum+i;
-        vector<vector<int>> dp(nums.size()+1,vector<int>((sum*2)+1,-1));
-        return healper(nums,target,0,0,dp,sum);
+        
+        int total=0;
+        for(auto i:nums){
+            total=total+i;
+        }
+        vector<vector<int>> dp(nums.size()+1,vector<int>((total*2)+1,-1));
+        return helper(nums,target,0,nums.size(),0,total,dp);
     }
 };
