484f69c7ec0a1547c7e5f10129f6cdb5b8af2439
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-12-27 10:17:16 +0530

    Time: 196 ms (30.44%) | Memory: 101 MB (21.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,22 +1,26 @@
+
+
 class Solution {
 public:
-    bool healper(int target,int idx,vector<int> &nums,vector<vector<int>> &dp){
-       if (target == 0) return true; 
-        if (idx == nums.size() || target < 0) return false;
-        if(dp[idx][target]!=-1) return dp[idx][target]; 
-        bool leave=healper(target,idx+1,nums,dp);
-        bool pick=healper(target-nums[idx],idx+1,nums,dp);
-        return dp[idx][target]=leave || pick;
+    bool helper(vector<int> &nums,int idx,int sum,int s,int Fsum,vector<vector<int>> &dp){
+        if(idx==s) return false;
+        if(Fsum>sum) return false;
+        if(Fsum==sum) return true;
+        if(dp[idx][Fsum]!=-1) return dp[idx][Fsum];
+        bool take=helper(nums,idx+1,sum,s,Fsum+nums[idx],dp);
+        bool leave=helper(nums,idx+1,sum,s,Fsum,dp);
+        return dp[idx][Fsum]=(take|| leave);
     }
     bool canPartition(vector<int>& nums) {
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        int s=nums.size();
+        
+       for(int i=0;i<nums.size();i++){
             sum=sum+nums[i];
-        }
-        if(sum%2!=0) return false;
-        int target=sum/2;
-        vector<vector<int>> dp(nums.size()+1,vector<int>(target+1,-1));
-        return healper(target,0,nums,dp);
-
+       } 
+       if(sum%2!=0) return false; 
+       int Fsum=0;
+       vector<vector<int>> dp(s + 1, vector<int>((sum / 2) + 1, -1));
+       return helper(nums,0,sum/2,s,Fsum,dp);
     }
 };
