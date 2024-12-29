bbc6423f853add5ccadf1a55d47f2ea032b2b28a
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2024-12-30 03:25:18 +0530

    Time: 163 ms (39.20%) | Memory: 101 MB (20.03%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -2,25 +2,26 @@
 
 class Solution {
 public:
-    bool helper(vector<int> &nums,int idx,int sum,int s,int Fsum,vector<vector<int>> &dp){
-        if(idx==s) return false;
-        if(Fsum>sum) return false;
-        if(Fsum==sum) return true;
-        if(dp[idx][Fsum]!=-1) return dp[idx][Fsum];
-        bool take=helper(nums,idx+1,sum,s,Fsum+nums[idx],dp);
-        bool leave=helper(nums,idx+1,sum,s,Fsum,dp);
-        return dp[idx][Fsum]=(take|| leave);
-    }
+
     bool canPartition(vector<int>& nums) {
-        int sum=0;
-        int s=nums.size();
-        
-       for(int i=0;i<nums.size();i++){
-            sum=sum+nums[i];
-       } 
-       if(sum%2!=0) return false; 
-       int Fsum=0;
-       vector<vector<int>> dp(s + 1, vector<int>((sum / 2) + 1, -1));
-       return helper(nums,0,sum/2,s,Fsum,dp);
+        int sum = 0;
+        int s = nums.size();
+
+        for (int i = 0; i < nums.size(); i++) {
+            sum = sum + nums[i];
+        }
+
+        if (sum % 2 != 0)
+            return false;
+        int halfSum = sum / 2;
+        vector<vector<int>> dp(s+1,vector<int>(halfSum+1));
+        for (int i = 0; i < s; i++) {
+            for (int j = 0; j <= halfSum; j++) {
+                bool take = (i>0) ? (j-nums[i]>=0) ? dp[i-1][j-nums[i]]==1 : j==0 : 0;
+                bool leave = (i>0) ? dp[i-1][j]==1 : j==0;
+                dp[i][j] = (take || leave);
+            }
+        }
+        return dp[s-1][halfSum]==1;
     }
 };
