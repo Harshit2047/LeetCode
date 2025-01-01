c32d4f0c521e01d2f076d7d179cd384e3dea9b2f
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-01-02 02:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (23.44%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -3,14 +3,14 @@ public:
     int maxScore(string s) {
         int countOne=0;
         for(auto i:s){
-            if(i=='1') countOne++;
+            if(i=='1') countOne++;  // make count of all the ones present in string 
         }
         int countZero=0;
         int ans=INT_MIN;
         for(int i=0;i<s.size()-1;i++){
-            if(s[i]=='1') countOne--;
-            else if(s[i]=='0') countZero++;
-            ans=max(ans,countOne+countZero);
+            if(s[i]=='1') countOne--; // then if 1 comes subtract it from countOne means now the we are on is come to left.
+            else if(s[i]=='0') countZero++; // if 0 comes add 1 to countZero means the zero we are on is on the left 
+            ans=max(ans,countOne+countZero); // find max of bot at each step 
         }
         return ans;
     }
