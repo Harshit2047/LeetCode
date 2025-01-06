bf1a6d77ca2b6da1d8f7838a9de52cfb873079c4
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-01-07 03:51:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (58.51%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -15,8 +15,7 @@ public:
     int checkBalance(TreeNode* root){
         if(!root)
             return 0;
-        if(!ans) 
-            return 0;
+        
         int leftSubTree = checkBalance(root->left);
         int rightSubTree = checkBalance(root->right);
         if(abs(leftSubTree-rightSubTree) > 1){
