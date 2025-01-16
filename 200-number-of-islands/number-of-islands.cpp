d3f66155736abf3debe72fcd6649d82637441a06
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-01-16 10:15:23 +0530

    Time: 23 ms (84.06%) | Memory: 16.7 MB (61.44%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid,vector<vector<bool>>&isVisited,pair<int,int> p){
-        queue<pair<int,int>> q;
-        q.push(p);
-        isVisited[p.first][p.second]=true;
-        while(q.size()>0){
-            pair<int,int> p=q.front();
-            q.pop();
-            int i=p.first;
-            int j=p.second;
-            if( i+1<grid.size() && !isVisited[i+1][j] && grid[i+1][j]=='1'){
-                q.push({i+1,j});
-                isVisited[i+1][j]=true;
-            }
-             if(i-1>=0 && !isVisited[i-1][j] && grid[i-1][j]=='1'){
-                q.push({i-1,j});
-                isVisited[i-1][j]=true;
-            }
-             if(j+1<grid[0].size() && !isVisited[i][j+1] && grid[i][j+1]=='1'){
-                q.push({i,j+1});
-                isVisited[i][j+1]=true;
-            }
-             if(j-1>= 0 && !isVisited[i][j-1] && grid[i][j-1]=='1'){
-                q.push({i,j-1});
-                isVisited[i][j-1]=true;
-            }
-            
+    void dfs(vector<vector<bool>> &isVisited,int i,int j,vector<vector<char>> &grid){
+        isVisited[i][j]=true;
+        if(i>=1 && grid[i-1][j]=='1' && isVisited[i-1][j]==false){
+            dfs(isVisited,i-1,j,grid);
+        }
+        if(i<grid.size()-1 && grid[i+1][j]=='1' && isVisited[i+1][j]==false){
+            dfs(isVisited,i+1,j,grid);
+        }
+        if(j>=1 && grid[i][j-1]=='1' && isVisited[i][j-1]==false){
+            dfs(isVisited,i,j-1,grid);
+        }
+        if(j<grid[0].size()-1 && grid[i][j+1]=='1' && isVisited[i][j+1]==false){
+            dfs(isVisited,i,j+1,grid);
         }
 
     }
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> isVisited(grid.size(),vector<bool>(grid[0].size(),false));
         int count=0;
+        vector<vector<bool>> isVisited(grid.size(),vector<bool>(grid[0].size(),false));
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(!isVisited[i][j] && grid[i][j]=='1'){
-                    pair<int,int> p={i,j};
-                    bfs(grid,isVisited,p);
+                if(grid[i][j]=='1' && isVisited[i][j]==false){
+                    dfs(isVisited,i,j,grid);
                     count++;
                 }
             }
