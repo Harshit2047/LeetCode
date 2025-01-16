12be93c7732f2456483935aeeb6f55c6c07caa3d
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-01-16 09:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (27.27%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -15,11 +15,20 @@ public:
             }
         }
     }
+    void dfs(vector<bool> &isVisited,vector<vector<int>> &rooms,int idx){
+        vector<int> temp=rooms[idx];
+        for(int i=0;i<temp.size();i++){
+            if(isVisited[temp[i]]==false){
+                isVisited[temp[i]]=true;
+                dfs(isVisited,rooms,temp[i]);
+            }
+        }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
         vector<bool> isVisited(n,false);
         isVisited[0]=true;
-        bfs(isVisited,rooms,0);
+        dfs(isVisited,rooms,0);
         for(int i=0;i<isVisited.size();i++){
             if( isVisited[i]==false) return false;
         }
