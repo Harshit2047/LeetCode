a3d264e15198181f9025dd160bf995747eabb32f
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-01-30 11:55:56 +0530

    Time: 1 ms (53.93%) | Memory: 19.4 MB (74.62%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    // void bfs(vector<bool> &isVisited,vector<vector<int>>& isConnected,int i){
-    //     int n=isConnected[0].size();
-    //     isVisited[i]=true;
-    //     queue<int> q;
-    //     q.push(i);
-    //     while(q.size()>0){
-    //         int temp=q.front();
-    //         q.pop();
-    //         for(int j=0;j<n;j++){
-    //             if(isVisited[j]!=true && isConnected[temp][j]==1){
-    //                 q.push(j);
-    //                 isVisited[j]=true;
-    //             }
-    //         }
-            
-    //     }
-    // }
-    void dfs(vector<bool> &isVisited,vector<vector<int>>& isConnected,int i){
-        isVisited[i]=true;
-        for(int j=0;j<isConnected[0].size();j++){
-            if(isConnected[i][j]==1 && isVisited[j]==false){
-                dfs(isVisited,isConnected,j);
+    vector<int> parent;
+    vector<int> size;
+    int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+    void unionSet(int a,int b){
+        int leaderA=find(a);
+        int leaderB=find(b);
+        if(leaderA!=leaderB){
+            if(size[leaderA]>size[leaderB]){
+                parent[leaderB]=leaderA;
+                size[leaderA]+=size[leaderB];
+            }
+            else{
+                parent[leaderA]=leaderB;
+                size[leaderB]+=size[leaderA];
             }
         }
     }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        int count=0;
-        vector<bool> isVisited(n,false);
-        for(int i=0;i<n;i++){
-            if(!isVisited[i]){
-                dfs(isVisited,isConnected,i);
-                count++;
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=1;i<parent.size();i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        for(int i=0;i<isConnected.size();i++){
+            for(int j=0;j<isConnected.size();j++){
+                if(i!=j && isConnected[i][j]==1){
+                    unionSet(i+1,j+1);
+                }
             }
+        }
+        int count=0;
+        for(int i=1;i<parent.size();i++){
+            if(parent[i]==i) count++;
         }
         return count;
     }
