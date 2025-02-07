f73fa7983bef049d014586a55b88d656cd9a2f36
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-02-07 11:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (9.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,7 +6,17 @@ public:
         return helper(n-1)+helper(n-2);
     }
     int fib(int n) {
+        // return helper(n);
+        vector<int> dp(n+1);
         if(n==0) return 0;
-        return helper(n);
+        if(n==1) return 1;
+        if(n==2) return 1;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
