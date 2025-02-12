3e0013034281b37a603887760c8999eb1e3cfafc
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-02-12 13:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (92.04%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                swap(arr[i][j], arr[j][i]);
+            }
+        }
+
+        for(int i =0;i<arr.size();i++){
+            reverse(arr[i].begin(),arr[i].end());
+        }
+    }
+};
