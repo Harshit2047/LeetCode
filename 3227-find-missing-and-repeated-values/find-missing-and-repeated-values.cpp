484f6942fcdfeed801ef6d598a292e3fca1dedaa
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-03-08 00:57:37 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (95.07%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int size = n * n;
-        vector<int> count(size + 1, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                count[grid[i][j]]++;
+        long long n=grid.size()*grid.size();
+        long long F=(n*(n+1))/2;
+        long long I=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                I+=grid[i][j];
             }
         }
-
-        int a = -1, b = -1;
-
-        for (int num = 1; num <= size; num++) {
-            if (count[num] == 2) {
-                a = num;
-            } else if (count[num] == 0) {
-                b = num;
+        long long SF=((n*(n+1)*((2*n)+1)))/6;
+        long long SI=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                SI+=pow(grid[i][j],2);
             }
         }
+        long long eq1=F-I;
+        long long eq2=(SF-SI)/eq1;
+        long long ansFirst=(eq2-eq1)/2;
+        long long ansSecond=eq1+ansFirst;
+        return {(int)ansFirst,(int)ansSecond};
 
-        return {a, b};
     }
 };
