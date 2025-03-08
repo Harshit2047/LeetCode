2c4a2584e70e5ebdb4f840a708be463dc0bad083
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-03-09 00:54:32 +0530

    Time: 3 ms (91.40%) | Memory: 14.9 MB (96.25%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -27,30 +27,44 @@ public:
         return hold->next;
     }
     Node* copyRandomList(Node* head) {
+        if(head==NULL) return NULL;
         Node* temp=head;
-        Node* ok=new Node(-1);
-        Node* holdok=ok;
-        unordered_map<Node*,Node*> mp;
-        while(temp){
-            Node* L=new Node(temp->val);
-            ok->next=L;
-            mp[temp]=L;
-            temp=temp->next;
-            ok=ok->next;
+        Node* t1=head;
+        Node* t2=Copy(temp);
+        Node* alt=new Node(-1);
+        Node* holdAlt=alt;
+        while(t1){
+            alt->next=t1;
+            t1=t1->next;
+            alt=alt->next;
+            alt->next=t2;
+            t2=t2->next;
+            alt=alt->next;
         }
-        holdok=holdok->next;
-        Node* ans=holdok;
-        temp=head;
-        while(temp){
-            if(temp->random==NULL){
-                holdok->random=NULL;
+        Node* cur=holdAlt->next;
+        t1=cur;
+        t2=cur->next;
+        while(cur){
+            if(cur->random==NULL){
+                cur->next->random=NULL;
             }
             else{
-                holdok->random=mp[temp->random];
+                cur->next->random=cur->random->next;
             }
-            temp=temp->next;
-            holdok=holdok->next;
+            cur=(cur->next==NULL) ? NULL : cur->next->next;
         }
-        return ans;
+        Node* holdt2=t2;
+        while(t1){
+            t1->next=(t1->next==NULL)? NULL :t1->next->next;
+            t1=t1->next;
+            t2->next=(t2->next==NULL)? NULL: t2->next->next;
+            t2=t2->next;
+        }
+        return holdt2;
+
+
+        
+
+        
     }
 };
