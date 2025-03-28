9e52cbbb20725e7f8cee8fcde674587229b12c08
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-03-28 19:53:10 +0530

    Time: 89 ms (42.31%) | Memory: 9.5 MB (74.46%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
+    bool isPalindrome(string &s,int i,int j){
+        if(i>=j) return true;
+        if(s[i]==s[j]) return isPalindrome(s,i+1,j-1);
+        else return false;
+    }
     string longestPalindrome(string s) {
-        vector<vector<int>> dp(s.length(), vector<int>(s.length(), 0));
-        int start = 0;
-        int end = 0;
-        int temp = INT_MIN;
-        for (int k = 0; k < s.length(); k++) {
-            int i = 0;
-            int j = k;
-
-            while (j < s.length()) {
-                if (i == j) {
-                    dp[i][j] = 1;
-                    if (temp < j - i) {
-                        temp = j - i;
-                        start = i;
-                        end = j;
-                    }
-
-                } else if (i == j - 1) {
-                    if (s[i] == s[j]) {
-                        dp[i][j] = 1;
-                        if (temp < j - i) {
-                            temp = j - i;
-                            start = i;
-                            end = j;
-                        }
-                    }
-                } else {
-                    if (s[i] == s[j]) {
-                        if (dp[i + 1][j - 1] == 1) {
-                            dp[i][j] = 1;
-                            if (temp < j - i) {
-                                temp = j - i;
-                                start = i;
-                                end = j;
-                            }
-                        }
-                    }
+        int maxLen=0;
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                if(maxLen<j-i+1 && isPalindrome(s,i,j)){
+                    maxLen=j-i+1;
+                    start=i;
+                    end=j;
                 }
-
-                i++;
-                j++;
+                
             }
         }
-        cout << start << " " << end;
-        string sub = s.substr(start, temp+1);
-        return sub;
+        return s.substr(start,end-start+1);
     }
 };
