bdb153701a011b9f0a993516e4ee0fe5a3dba0c5
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-03-29 18:19:26 +0530

    Time: 2 ms (13.18%) | Memory: 10.1 MB (7.99%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[') st.push(s[i]);
+            else{
+                if(s[i]==')' && st.size() && st.top()=='(') st.pop();
+                else if(s[i]==']' && st.size() && st.top()=='[') st.pop();
+                else if(s[i]=='}' && st.size() && st.top()=='{') st.pop();
+                else return false;
+            }
+        }
+        if(st.size()==0) return true;
+        else return false;
+    }
+};
