314f7ae4271197c2893faf386d56e57f75b3283d
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-03-31 23:04:57 +0530

    Time: 20 ms (36.66%) | Memory: 47.1 MB (79.31%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
+int MOD=1e9+7;
     int sumSubarrayMins(vector<int>& arr) {
-        int s = arr.size();
+        int s=arr.size();
         vector<int> pse(s);
         stack<int> st;
         st.push(0);
-        pse[0] = -1;
-
-        for (int i = 1; i < arr.size(); i++) {
-            while (st.size() && arr[st.top()] > arr[i]) {
+        pse[0]=-1;
+        for(int i=1;i<arr.size();i++){
+            while(st.size() && arr[st.top()]>arr[i]){
                 st.pop();
             }
-            if (st.size() != 0) pse[i] = st.top();
-            else pse[i] = -1;
+            if(st.size()!=0) pse[i]=st.top();
+            else pse[i]=-1;
             st.push(i);
         }
-
-        while (!st.empty()) {
+        
+        while(st.size()){
             st.pop();
         }
-
         vector<int> nxe(s);
-        nxe[s - 1] = s;
-        st.push(s - 1);
-
-        for (int i = s - 2; i >= 0; i--) {
-            while (st.size() && arr[st.top()] >= arr[i]) {  // Fix: Use `>=` to handle duplicates
+        nxe[s-1]=s;
+        st.push(s-1);
+        for(int i=s-2;i>=0;i--){
+            while(st.size() && arr[st.top()]>=arr[i]){
                 st.pop();
             }
-            if (st.size() != 0) nxe[i] = st.top();
-            else nxe[i] = s;
+            if(st.size()!=0) nxe[i]=st.top();
+            else nxe[i]=s;
             st.push(i);
         }
-
-        int ans = 0;
-        int MOD = 1e9 + 7;  // Since the problem requires modulo operation
-
-        for (int i = 0; i < arr.size(); i++) {
+        
+      
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
             int left = i - pse[i];
             int right = nxe[i] - i;
-            ans = (ans + ((long long)left * right % MOD) * arr[i] % MOD) % MOD; // Fix contribution formula
+            ans = (ans + ((long long)left * right % MOD) * arr[i] % MOD) % MOD;
         }
-
         return ans;
     }
 };
