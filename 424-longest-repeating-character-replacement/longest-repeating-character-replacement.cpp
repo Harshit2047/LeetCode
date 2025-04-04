37dc0f867480d138c278889e610ab32f01ec0d53
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-04-05 01:51:31 +0530

    Time: 20 ms (11.79%) | Memory: 10.9 MB (43.17%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int i=0;
+        int j=0;
+        int ans=0;
+        int maxF=0;
+        unordered_map<char,int> mp;
+        while(j<s.length()){
+            mp[s[j]]++;
+            maxF=max(maxF,mp[s[j]]);
+            if((j-i+1)-maxF<=k) ans=max(ans,j-i+1);
+            while((j-i+1)-maxF>k){
+                mp[s[i]]--;
+                for(auto i:mp){
+                    maxF=max(maxF,i.second);
+                }
+                i++;
+            } 
+            j++;
+        }
+        return ans;
+    }
+};
