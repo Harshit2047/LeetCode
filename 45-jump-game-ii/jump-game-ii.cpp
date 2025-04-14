a39eda2b25779fe36a1e890df66e352ace59eb09
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-04-14 18:52:09 +0530

    Time: 3 ms (31.24%) | Memory: 20.5 MB (35.66%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,30 +1,19 @@
-#include <climits>
 class Solution {
 public:
-    int helper(vector<int> &nums, int idx, vector<int> &dp) {
-        if (idx >= nums.size() - 1) return 0;
-        if (dp[idx] != -1) return dp[idx];
-        int ans = INT_MAX;
-        for (int i = 1; i <= nums[idx]; i++) {
-            int count = helper(nums, idx + i, dp);
-            if (count != INT_MAX) {
-                ans = min(ans, 1 + count);
-            }
-        }
-        return dp[idx] = ans;
-    }
-
     int jump(vector<int>& nums) {
-        if (nums.size() == 1) return 0;
-        int range = nums[0];
-        int ans = INT_MAX;
-        vector<int> dp(nums.size(), -1);
-        for (int i = 1; i <= range; i++) {
-            int count = helper(nums, i, dp);
-            if (count != INT_MAX) {
-                ans = min(ans, 1 + count);
+        if(nums.size()==1) return 0;
+        int low=0;
+        int high=0;
+        int count=0;
+        while(high<nums.size()-1){
+            int RangeMax=INT_MIN;
+            for(int i=low;i<=high;i++){
+                RangeMax=max(RangeMax,i+nums[i]);
             }
+            low=high+1;
+            high=RangeMax;
+            count++;
         }
-        return ans;
+        return count;
     }
 };
