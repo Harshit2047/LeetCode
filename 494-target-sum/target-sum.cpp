e057c7bf38b03b78377a2ec2fd23213ff4844bf4
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-05-25 00:45:29 +0530

    Time: 926 ms (7.28%) | Memory: 11.6 MB (82.64%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int helper(vector<int> &nums,int target,int idx,int size,int sum,int total,vector<vector<int>> &dp){
-        if(idx==size){
-            if(target==sum) return 1;
+    int helper(vector<int>&nums,int target,int idx){
+        if(idx==nums.size()){
+            if(target==0) return 1;
             else return 0;
         }
-        if(dp[idx][sum+total]!=-1) return dp[idx][sum+total];
-        int postive=helper(nums,target,idx+1,size,sum+nums[idx],total,dp);
-        int negative=helper(nums,target,idx+1,size,sum-nums[idx],total,dp);
-        return dp[idx][sum+total]=(negative+postive);
+        int neg=helper(nums,target-nums[idx],idx+1);
+        int plus=helper(nums,target+nums[idx],idx+1);
+        return neg+plus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        int total=0;
-        for(auto i:nums){
-            total=total+i;
-        }
-        vector<vector<int>> dp(nums.size()+1,vector<int>((total*2)+1,-1));
-        return helper(nums,target,0,nums.size(),0,total,dp);
+        return helper(nums,target,0);
     }
 };
