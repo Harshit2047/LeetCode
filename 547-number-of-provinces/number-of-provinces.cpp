3e8ccf99ad462aa5fd2c9207b79bc93031a6d146
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-06-12 20:01:07 +0530

    Time: 3 ms (43.14%) | Memory: 21.2 MB (13.63%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> size;
-    int find(int a){
-        if(parent[a]==a) return a;
-        return parent[a]=find(parent[a]);
-    }
-    void unionSet(int a,int b){
-        int leaderA=find(a);
-        int leaderB=find(b);
-        if(leaderA!=leaderB){
-            if(size[leaderA]>size[leaderB]){
-                parent[leaderB]=leaderA;
-                size[leaderA]+=size[leaderB];
-            }
-            else{
-                parent[leaderA]=leaderB;
-                size[leaderB]+=size[leaderA];
-            }
-        }
-    }
-
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        parent.resize(n+1);
-        size.resize(n+1);
-        for(int i=1;i<parent.size();i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-        for(int i=0;i<isConnected.size();i++){
-            for(int j=0;j<isConnected.size();j++){
-                if(i!=j && isConnected[i][j]==1){
-                    unionSet(i+1,j+1);
+    void helper(int i,vector<int> &isVisted,vector<vector<int>> &isConnected) {
+        queue<int> q;
+        q.push(i);
+        while(q.size()){
+            int idx=q.front();
+            q.pop();
+            vector<int> temp= isConnected[idx-1];
+            for(int i=0;i<temp.size();i++){
+                if(temp[i]==1 && isVisted[i+1]==false){
+                    q.push(i+1);
+                    isVisted[i+1]=true;
                 }
             }
         }
-        int count=0;
-        for(int i=1;i<parent.size();i++){
-            if(parent[i]==i) count++;
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int count = 0;
+        int size = isConnected.size();
+        vector<int> isVisted(size+1,false);
+        for (int i = 1; i <= isConnected.size(); i++) {
+            if (isVisted[i]==false) {
+                helper(i,isVisted,isConnected);
+                count++;
+            }
         }
         return count;
     }
