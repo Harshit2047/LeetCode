8b1141d2b8d9c87e14a218ee97367557dd6aa784
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-06-17 10:23:55 +0530

    Time: 1236 ms (9.87%) | Memory: 295.6 MB (8.57%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int count=0;
-        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(int i=0;i<nums.size();i++){
-           
-            
-                int val1=nums[i];
-                int j=i+1;
-                int k=nums.size()-1;
-                while(j<k){
-                    int sum=val1+nums[j]+nums[k];
-                    if(sum==0){
-                        vector<int> temp{val1,nums[j],nums[k]};
-                        ans.push_back(temp);
-                        while(i<nums.size()-2 && nums[i]==nums[i+1])i++;
-                        while(j<k && nums[j]==nums[j+1])j++;
-                        while(k>j && nums[k]==nums[k-1])k--;
-                        j++;
-                        k--;
-
-                    }
-                    else if(sum>0) k--;
-                    else if(sum<0) j++;
+        set<vector<int>> st;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-2;i++){
+            int target=0-nums[i];
+            int low=i+1;
+            int high=nums.size()-1;
+            while(low<high){
+                if(nums[low]+nums[high]==target){
+                    st.insert({nums[i],nums[low],nums[high]});
+                    low++;
+                    high--;
                 }
-            
+                else if(nums[low]+nums[high]<target){
+                    low++;
+                }
+                else{
+                    high--;
+                }
+            }
+        }
+        for(auto i:st){
+            ans.push_back(i);
         }
         return ans;
     }
