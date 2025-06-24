679cec81c0092025c316394f78428ba2e7819d44
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-06-24 11:52:16 +0530

    Time: 10 ms (44.21%) | Memory: 20.9 MB (26.29%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,58 +1,48 @@
 class Solution {
 public:
-    int helper(vector<int> &v){
-        
-        int n=v.size();
-        vector<int> nsi(n);
-        vector<int> psi(n);
-        nsi[n-1]=n;
+    int healper(vector<int> heights){
+        int n=heights.size();
         stack<int> st;
         st.push(n-1);
+        vector<int> nsi(n);
+        nsi[n-1]=n;
         for(int i=n-2;i>=0;i--){
-            while(st.size() && (v[st.top()])>=v[i]){
-                st.pop();
-            }
+            while(st.size() && heights[st.top()]>=heights[i]) st.pop();
             if(st.size()==0) nsi[i]=n;
             else nsi[i]=st.top();
             st.push(i);
         }
         while(st.size()) st.pop();
+        vector<int> psi(n);
         psi[0]=-1;
         st.push(0);
         for(int i=1;i<n;i++){
-            while(st.size() && v[st.top()]>=v[i]){
-                st.pop();
-            }
+            while(st.size() && heights[st.top()]>heights[i]) st.pop();
             if(st.size()==0) psi[i]=-1;
             else psi[i]=st.top();
             st.push(i);
         }
-        int ans=0;
+        int ans=INT_MIN;
         for(int i=0;i<n;i++){
-            int breath=nsi[i]-psi[i]-1;
-            int hight=v[i];
-             ans=max(ans,breath*hight);
+            int width=nsi[i]-psi[i]-1;
+            ans=max(ans,width*heights[i]);
         }
         return ans;
+        
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<vector<int>> v(n,vector<int>(m));
-        for(int i=0;i<matrix[0].size();i++){
-            for(int j=0;j<matrix.size();j++){
-                if(j==0) v[j][i]=matrix[j][i]-'0';
-                else{
-                    if(matrix[j][i]-'0'==0) v[j][i]=0;
-                    else v[j][i]=v[j-1][i]+matrix[j][i]-'0';
-                }
-            }
-        }
+        vector<int> temp(m,0);
         int ans=0;
         for(int i=0;i<n;i++){
-            ans=max(ans,helper(v[i]));
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0') temp[j]=0;
+                else temp[j]+=1;
+            }
+            ans=max(ans,healper(temp));
         }
+    
         return ans;
-        
     }
 };
