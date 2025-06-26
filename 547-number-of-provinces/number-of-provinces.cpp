5fb6ed49e7f80b4e36d0d1c98a4c6c9587c644ac
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-06-26 20:23:06 +0530

    Time: 8 ms (12.32%) | Memory: 20 MB (52.83%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,55 +1,30 @@
 class Solution {
 public:
-    // void helper(int i,vector<int> &isVisted,vector<vector<int>> &isConnected)
-    // {
-    //     queue<int> q;
-    //     q.push(i);
-    //     while(q.size()){
-    //         int idx=q.front();
-    //         q.pop();
-    //         vector<int> temp= isConnected[idx-1];
-    //         for(int i=0;i<temp.size();i++){
-    //             if(temp[i]==1 && isVisted[i+1]==false){
-    //                 q.push(i+1);
-    //                 isVisted[i+1]=true;
-    //             }
-    //         }
-    //     }
-    // }
-    // void dfs(int idx,vector<vector<int>> &isConnected,vector<int> &isVisted){
-    //     if(isVisted[idx]==true || idx>isConnected.size()-1) return;
-    //     vector<int> temp=isConnected[idx];
-    //     for(int i=0;i<temp.size();i++){
-    //         if(isVisted[i]==false && temp[i]==1){
-    //              isVisted[i]=true;
-    //             dfs(i,isConnected,isVisted);
-
-    //         }
-    //     }
-    // }
-
-    void dfs(int u, vector<vector<int>>& isConnected, vector<int>& isVisted) {
-        if (isVisted[u])
-            return;
-        isVisted[u] = true;
-
-        for (int v = 0; v < isConnected[u].size(); ++v) {
-        if (isConnected[u][v] == 1 && !isVisted[v]) {
-            dfs(v, isConnected, isVisted);
+    void helper(vector<vector<int>> &isConnected,vector<bool> &isVisited,int idx){
+        queue<int> q;
+        q.push(idx);
+        while(q.size()){
+            int currIdx=q.front();
+            isVisited[currIdx]=true;
+            q.pop();
+            for(int i=0;i<isConnected[currIdx].size();i++){
+                if(isConnected[currIdx][i]==1 && isVisited[i]==false){
+                    q.push(i);
+                }
+            }
         }
     }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int count = 0;
-        int size = isConnected.size();
-        vector<int> isVisted(size + 1, false);
-
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (isVisted[i] == false) {
-                dfs(i, isConnected, isVisted);
+        int n=isConnected.size();
+        vector<bool> isVisited(n,false);
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(isVisited[i]==false){
+                helper(isConnected,isVisited,i);
                 count++;
             }
         }
         return count;
+
     }
 };
