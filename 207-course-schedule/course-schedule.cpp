1f1dbf02c158372cc7555fac4aaf46704af7d2a0
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-06-30 23:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.39%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &adj,vector<bool> &isVisited,int start,vector<bool> &path){
-        isVisited[start]=true;
-        path[start]=true;
-        for(int i=0;i<adj[start].size();i++){
-            int neighbor=adj[start][i];
-            if(isVisited[neighbor]==false){
-                if(dfs(adj,isVisited,neighbor,path)){
+    bool dfs(vector<vector<int>>& adj, vector<bool>& isVisited, int start,
+             vector<bool>& path) {
+        isVisited[start] = true;
+        path[start] = true;
+        for (int i = 0; i < adj[start].size(); i++) {
+            int neighbor = adj[start][i];
+            if (isVisited[neighbor] == false) {
+                if (dfs(adj, isVisited, neighbor, path)) {
                     return true;
                 }
-            }
-            else if(isVisited[neighbor] && path[neighbor]) return true;
-    
+            } else if (isVisited[neighbor] && path[neighbor])
+                return true;
         }
-        path[start]=false;
+        path[start] = false;
         return false;
     }
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-         vector<vector<int>> adj(n, vector<int>());
+        vector<vector<int>> adj(n, vector<int>());
         vector<bool> isVisited(n, false);
         for (int i = 0; i < prerequisites.size(); i++) {
             adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
         }
         vector<bool> path(n, false);
         for (int i = 0; i < n; i++) {
-            if (!isVisited[i]) {  // Check if the node is not visited
-                if (dfs(adj, isVisited, i, path)) {  // Start DFS from each unvisited node
-                    return false;  // Cycle detected
+            if (!isVisited[i]) {
+                if (dfs(adj, isVisited, i, path)) {
+                    return false;
                 }
             }
         }
-        return true; 
+        return true;
     }
 };
