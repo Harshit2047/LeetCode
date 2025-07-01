8363ff127b412fa57fc69c7d1dbdea9e3bf088a5
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-07-01 11:07:48 +0530

    Time: 183 ms (13.99%) | Memory: 46.4 MB (15.69%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    struct compare{
-        bool operator()(const vector<int> &a,const vector<int> &b){
-            if(a[2]==b[2]) return a[0]>b[0];
-            return a[2]>b[2];
-        }
-    };
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        vector<vector<int>> dir{{-1,0},{1,0},{0,-1},{0,1}};
-        vector<vector<int>> costArr(heights.size(),vector<int>(heights[0].size(),INT_MAX));
-        priority_queue<vector<int>,vector<vector<int>>,compare> pq;
-        costArr[0][0]=0;
-        pq.push({0,0,0});
-        while(pq.size()){
-            vector<int> temp=pq.top();
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+       
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = 0;
+
+       
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, 0, 0});
+
+        vector<vector<int>> temp = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        while (!pq.empty()) {
+            vector<int> xd = pq.top();
             pq.pop();
-            int i=temp[0];
-            int j=temp[1];
-            int diff=temp[2];
-            for(int x=0;x<dir.size();x++){
-                int i_=i+dir[x][0];
-                int j_=j+dir[x][1];
 
-                if (i_ >= 0 && i_ < heights.size() && j_ >= 0 && j_ < heights[0].size()){
-                    int currDiff=abs(heights[i_][j_]-heights[i][j]);
-                    int val=max(diff,currDiff);
-                    if(costArr[i_][j_]>val){
-                        costArr[i_][j_]=val;
-                        pq.push({i_,j_,costArr[i_][j_]});
+            int diff = xd[0];
+            int x = xd[1];
+            int y = xd[2];
+
+           
+            if (x == n - 1 && y == m - 1) return diff;
+
+            for (int i = 0; i < temp.size(); i++) {
+                int x_ = x + temp[i][0];
+                int y_ = y + temp[i][1];
+
+                if (x_ >= 0 && y_ >= 0 && x_ < n && y_ < m) {
+                    int effort = max(diff, abs(grid[x_][y_] - grid[x][y]));
+                    if (effort < dist[x_][y_]) {
+                        dist[x_][y_] = effort;
+                        pq.push({effort, x_, y_});
                     }
-
                 }
             }
         }
-        
-        return costArr[heights.size()-1][heights[0].size()-1];
+
+        return -1; 
     }
 };
