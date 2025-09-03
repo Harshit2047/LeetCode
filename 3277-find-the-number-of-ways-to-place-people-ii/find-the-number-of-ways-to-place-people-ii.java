5beac4301955e2bbdccf6e89be1781dd55b8159e
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-09-03 14:21:41 +0530

    Time: 60 ms (84.06%) | Memory: 45 MB (89.86%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.java
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.java
@@ -2,19 +2,20 @@ class Solution {
     public int numberOfPairs(int[][] points) {
         Arrays.sort(points,(a,b)->{
             if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
-            else return Integer.compare(b[1],a[1]);
+            else  return Integer.compare(b[1],a[1]);
         });
-
-        int n=points.length,count=0;
-        for(int i=0;i<n;i++){
-            int x=points[i][0],y=points[i][1];
-            int miny=Integer.MAX_VALUE;
+        int count=0;
+        for(int i=0;i<points.length;i++){
+            int curX=points[i][0];
+            int curY=points[i][1];
+            int height=Integer.MAX_VALUE;
             for(int j=i-1;j>=0;j--){
-                int x1=points[j][0],y2=points[j][1];
-                if(y2<y) continue;
-                if(y2<miny){
+                int prevX=points[j][0];
+                int prevY=points[j][1];
+                if(prevY < curY) continue;
+                if(prevY<height){
                     count++;
-                    miny=y2;
+                    height=prevY;
                 }
 
             }
