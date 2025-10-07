dc6a48d0153b8f3f676e74ab3afa4bcbbaa45a04
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-10-07 19:22:17 +0530

    Time: 88 ms (8.63%) | Memory: 72.3 MB (5.34%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void helper(vector<int> &arr,int target,int idx,vector<int> ans,vector<vector<int>> &finalans){
-        
-            if(target==0){
-                finalans.push_back(ans);
-                return;
-            } 
-            if(idx>=arr.size() || target<0){
-                return;
-            }
-        
-            helper(arr,target,idx+1,ans,finalans);
-            ans.push_back(arr[idx]);
-            helper(arr,target-arr[idx],idx,ans,finalans);
-       
+    void helper(vector<int>& candidates,int idx,int target,vector<int> temp,vector<vector<int>> &ans){
+        if(target<0 || idx==candidates.size()) return;
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        helper(candidates,idx+1,target,temp,ans);
+        temp.push_back(candidates[idx]);
+        helper(candidates,idx,target-candidates[idx],temp,ans);
+
     }
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>> finalans;
-        vector<int> ans;
-        helper(arr,target,0,ans,finalans);
-        return finalans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        helper(candidates,0,target,temp,ans);
+        return ans;
     }
 };
