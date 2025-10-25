543de250e6d6b3dd86ef9945e39bcc3d1f69221f
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-10-25 12:33:37 +0530

    Time: 540 ms (27.80%) | Memory: 254.8 MB (24.22%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    int nextBeautifulNumber(int n) {
-        vector<int> list;
-        vector<int> count(10, 0);
-        generate(0, count, list);
-        sort(list.begin(), list.end());
-        for (int num : list) {
-            if (num > n) return num;
+    bool helper(int n){
+        vector<int> v(10,0);
+        while(n){
+            v[n%10]++;
+            n=n/10;
         }
-        return -1;
-    }
-
-private:
-    void generate(long num, vector<int>& count, vector<int>& list) {
-        if (num > 0 && isBeautiful(count)) {
-            list.push_back((int)num);
-        }
-        if (num > 1224444) return;
-
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] < d) {
-                count[d]++;
-                generate(num * 10 + d, count, list);
-                count[d]--;
-            }
-        }
-    }
-
-    bool isBeautiful(const vector<int>& count) {
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] != 0 && count[d] != d)
-                return false;
+        for(int i=0;i<10;i++){
+            if(v[i]!=0 && v[i]!=i) return false;
         }
         return true;
+    }
+    int nextBeautifulNumber(int n) {
+        for(int i=n+1;;i++){
+            if(helper(i)) return i;
+        }
+        return -1;
     }
 };
