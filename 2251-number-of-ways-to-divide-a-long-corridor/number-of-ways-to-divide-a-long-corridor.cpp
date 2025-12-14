1e116d4460748638cd41526692dfb56f96834ec3
Harshit2047 <113328500+Harshit2047@users.noreply.github.com>
2025-12-14 21:27:30 +0530

    Time: 28 ms (51.50%) | Memory: 58.8 MB (26.50%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-const int M=1e9+7;
-    int helper(string &s,int idx,int count,vector<vector<int>> &dp){
-        if(idx==s.length() && count==2) return 1;
-        if(idx==s.length()) return 0;
-        if(dp[idx][count]!=-1) return dp[idx][count];
-        int take=0;
-        if(count==2) {
-            if(s[idx]=='S') return dp[idx][count]= helper(s,idx+1,1,dp);
-            else return dp[idx][count]= (helper(s,idx+1,0,dp)+helper(s,idx+1,count,dp))%M;
+    const int M=1e9+7;
+    int numberOfWays(string s) {
+        vector<int> idx;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='S'){
+                idx.push_back(i);
+            }
         }
-        else if(s[idx]=='S'){
-           take= helper(s,idx+1,count+1,dp);
+
+        if(idx.size()==0 || idx.size()==1 || idx.size()%2!=0) return 0;
+        if(idx.size()==2) return 1;
+
+        int prevIdx=1;
+        int nextIdx=2;
+        long long ans=1;
+        while(nextIdx<idx.size()){
+            ans = (ans * (idx[nextIdx] - idx[prevIdx])) % M;
+            prevIdx+=2;
+            nextIdx+=2;
         }
-        else {
-            take=helper(s,idx+1,count,dp);
-        }
-        return dp[idx][count]=take;
-    }
-    int numberOfWays(string corridor) {
-        int n=corridor.size();
-        vector<vector<int>> dp(n,vector<int>(3,-1));
-        return helper(corridor,0,0,dp);
+        return (int)ans;
     }
 };
